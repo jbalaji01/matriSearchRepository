acac8814fbebef94a343d725d148a62fc29620c2
jbalaji01 <j_balaji01@yahoo.com>
2020-08-22 21:58:16 +0530

    added webmaster and techno
    two new roles added

--- src/main/java/com/neemshade/matri/security/AuthoritiesConstants.java
+++ src/main/java/com/neemshade/matri/security/AuthoritiesConstants.java
@@ -7,6 +7,10 @@ public final class AuthoritiesConstants {
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
+    
+    public static final String WEBMASTER = "ROLE_WEBMASTER";
+    
+    public static final String TECHNO = "ROLE_TECHNO";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
